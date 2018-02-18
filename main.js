dab317aa35ba0b6bddabcb645df87cd0d813159b
Panupong Pasupat <ppasupat@stanford.edu>
2018-02-17 21:27:38 -0800

    Two items!!!

--- main.js
+++ main.js
@@ -17,8 +17,19 @@ function switchScene(target) {
   $('#scene-' + target).show();
 }
 
-$('.nav').click(function (event) {
+$('.nav').click(function () {
   switchScene($(this).attr('id').split('-')[2]);
+});
+
+$('.item').click(function () {
+  var x = $(this);
+  if (!x.hasClass('taken') || x.hasClass('used')) return;
+  if (x.hasClass('selected')) {
+    x.removeClass('selected');
+  } else {
+    $('.item').removeClass('selected');
+    x.addClass('selected');
+  }
 });
 
 // ################################
@@ -35,7 +46,26 @@ $('#o-start').click(function () {
 
 // ################################
 // kitchen
+
+$('#o-cupboard').click(function () {
+  $(this).toggleClass('open');
+  $('#o-sugar').toggleClass('reachable');
+});
+
+$('#o-sugar').click(function () {
+  $(this).addClass('taken');
+  $('#i-sugar').addClass('taken');
+});
   
+$('#o-fridge').click(function () {
+  $(this).toggleClass('open');
+  $('#o-butter').toggleClass('reachable');
+});
+
+$('#o-butter').click(function () {
+  $(this).addClass('taken');
+  $('#i-butter').addClass('taken');
+});
 
 
 
