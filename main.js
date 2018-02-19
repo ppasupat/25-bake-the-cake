06713a3fdb2cd0453d00b6291cb63b41ea60e8b5
Panupong Pasupat <ppasupat@stanford.edu>
2018-02-18 20:41:56 -0800

    Try this again

--- main.js
+++ main.js
@@ -327,7 +327,7 @@ function resizeScreen() {
   var sW = window.screen.width, sH = window.screen.height;
   var ratio = Math.min(sW / 800, sH / 500);
   if (ratio < 1) {
-    $('#viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes');
+    $('#viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes, width=' + (ratio * 800));
   } else {
     $('#viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=yes, width=800');
   }
